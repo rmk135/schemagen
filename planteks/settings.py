285ca889a3e967cdaeb6aa26d24cce548b3043b5
Vera Vovk <vovkverait@gmail.com>
2021-01-06 13:38:25 +0200

    Add if DEBUG

--- planteks/settings.py
+++ planteks/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-django_heroku.settings(locals())
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+   django_heroku.settings(locals())
