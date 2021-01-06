da9c29cb42596d96ebeebbd2c13f0ed109fc8ed0
Vera Vovk <vovkverait@gmail.com>
2021-01-06 18:10:23 +0200

    Fix in settings

--- planteks/settings.py
+++ planteks/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-#import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -133,6 +133,6 @@ if DEBUG:
    ]
 else:
    STATIC_ROOT = os.path.join(BASE_DIR,'static')
-   #django_heroku.settings(locals())
+   django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
