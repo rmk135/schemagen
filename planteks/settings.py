ac8cc97face104fefa62e68c9af0efa7b61ef61c
Vera Vovk <vovkverait@gmail.com>
2021-01-07 19:11:43 +0200

    Add DB connection

--- planteks/settings.py
+++ planteks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 #import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -111,6 +112,8 @@ DATABASES = {
 
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
